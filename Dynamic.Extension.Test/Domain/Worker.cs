0c3c82bd1c51b1f9f5f08c45127be137bf631d78
manueldiego <chico_herbal@hotmail.com>
2014-11-06 00:03:00 +0100

    New test for dynamic behavior.

---
+++ Dynamic.Extension.Test/Domain/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dynamic.Extension.Test.Domain
+{
+    public class Worker
+        : DynamicBinder
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+    }
+}
--- Dynamic.Extension.Test/DynamicTester.cs
+++ Dynamic.Extension.Test/DynamicTester.cs
@@ -2,11 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Dynamic.Extension.Test.Domain;
+using Xunit;
 
 namespace Dynamic.Extension.Test
 {
     public class DynamicTester
     {
+        [Fact]
+        public void AddNewMembersWithProp()
+        {
+            dynamic instance = new Worker
+                {
+                    Id = 5,
+                    Name = "MyName",
+                    Salary = 12000
+                };
 
+            instance.SalaryByYear = instance.Salary * 12;
+            instance.Surname = "MySurname";
+
+            Assert.NotNull(instance.Surname);
+            Assert.Equal(instance.SalaryByYear, instance.Salary * 12);
+        }
+
+
+        [Fact]
+        public void AddNewMembersWithNaming()
+        {
+            dynamic instance = new Worker
+            {
+                Id = 5,
+                Name = "MyName",
+                Salary = 12000
+            };
+
+            instance.SalaryByYear = instance.Salary * 12;
+            instance.Surname = "MySurname";
+
+            Assert.Equal(instance.Surname, instance["Surname"]);
+            Assert.Equal(instance.SalaryByYear, instance["SalaryByYear"]);
+        }
     }
 }
